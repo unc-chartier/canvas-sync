79703c4f772c580f667775eeec128793e3105fbe
unc-chartier <micah.chartier@unco.edu>
2016-03-22 08:39:05 -0600

    Added logging to the file.

--- canvas_transfer.py
+++ canvas_transfer.py
@@ -2,32 +2,50 @@
 
 import requests
 import json
+import logging
+import time
 from pprint import pprint
 
 
 # 1. Define inputs for the POST/GET request
 # Example: https://ian.test.instructure.com/api/v1/accounts/'
 base_url = 'https://unco.test.instructure.com/api/v1/accounts/self/'
-header = {'Authorization': 'Bearer 7227~eza9khb9zEVU7OngSVHZePvcF73TGjrMNksFjvIxW5J7W0IiQAlc4Fd5cDPDpXHF'}
+header = {'Authorization': 'Bearer 7227~8qA5twNyOirR9ePFsPQ3cal7ZVlK5SghvznnS8onnRYG3eUHmYaqr84NzzPNToIe'}
 
 # Parameters specific to the initial POST request
 # Example: 'SIS_Testing/users.csv'
-myfile = 'users_export.csv'
+myfile = 'inst_role_export.csv'
 payload = {'import_type': 'instructure_csv', 'extension': 'csv'}
 data = open(myfile, 'rb').read()
 
 # If you're checking the status of an import, include the sis_import_id here in quotes.
 # For submitting an import leave at None.
-import_id = '5'#None
+#import_id = None
+import_id = '9'
 
+
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.INFO)
+
+timestr = time.strftime("%H:%M:%S-%Y%m%d")
+logfile_name = timestr + ".log"
+#print(logfile_name)
+handler = logging.FileHandler(logfile_name)
+handler.setLevel(logging.INFO)
+formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+handler.setFormatter(formatter)
+logger.addHandler(handler)
+logger.info("Begin Logging")
 
 # 2. Create a response object from the POST request
 def myrequest(base_url, header, payload, data):
     if not import_id:
         print("Sending file: ", myfile)
+        logger.info('Sending file: %s', myfile)
         r = requests.post(base_url + "/sis_imports/", headers=header, params=payload, data=data)
     else:
         print("Import ID: ", import_id)
+        logger.info('Checking on the status of import %s', import_id)
         r = requests.get(base_url + "/sis_imports/" + import_id, headers=header)
     return r
 
@@ -49,6 +67,10 @@ def main():
     print("Printing rjson response")
     pprint(rjson)
     print rjson.get("workflow_state", "none")
+    for x in rjson.get("processing_warnings", "none"):
+        logger.warning(x)
+        print(x)
+    logger.info('End Logging')
 
 if __name__ == "__main__":
     main()
